b4056c01ed520b1ba626a02efce367c6610ef108
Heng Wang <kevinwang264@gmail.com>
2019-08-24 12:26:00 -0400

    add solution

--- algorithms/python/RemoveKDigits/RemoveKDigits.py
+++ algorithms/python/RemoveKDigits/RemoveKDigits.py
@@ -2,14 +2,30 @@ class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
         if k == 0:
             return num
-        n = int(num)
-        if n == 0:
+        if int(num) == 0 or len(num) == k:
             return '0'
+        result = []
+        for n in num:
+            while k > 0 and result and result[-1] > n:
+                result.pop()
+                k -= 1
+            result.append(n)
+        # After we went through the whole num string, if k stil greater than 0,
+        # meaning we still need to remove k more digits from the stack
+        # Since our stack already ordered, we just need to remove last k digits from the stack
+        if k > 0:
+            result = result[:-k]
+        return ''.join(result).lstrip('0') or '0'
 
 
+a = Solution().removeKdigits("1432219", 3)
+print(a)
+# expected: '1219'
 
+a = Solution().removeKdigits("10200", 1)
+print(a)
+# expected: '200'
 
-
-a = Solution().removeKdigits("123", 1)
-# print(a)
-
+a = Solution().removeKdigits("1234506789", 9)
+print(a)
+# expected: '0'
