742a54d30263f63ad20a6a7efb6b9bba3aa499b3
Heng Wang <kevinwang264@gmail.com>
2019-08-29 23:29:26 -0400

    update solution

--- algorithms/python/MajorityElement/MajorityElement.py
+++ algorithms/python/MajorityElement/MajorityElement.py
@@ -4,11 +4,8 @@ class Solution:
     def majorityElement_hashtable(self, nums: List[int]) -> int:
         d = {}
         for n in nums:
-            if n in d:
-                d[n] += 1
-            else:
-                d[n] = 1
-        
+            d[n] = d[n]+1 if n in d else 1
+
         for k in d.keys():
             if d[k] > len(nums)/2:
                 return k
