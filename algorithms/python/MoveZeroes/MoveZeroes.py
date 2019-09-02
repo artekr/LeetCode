b6676051c59f7233f1dea4a382acbf3ce80c39ca
Heng Wang <kevinwang264@gmail.com>
2019-09-01 21:46:55 -0400

    updated solution

--- algorithms/python/MoveZeroes/MoveZeroes.py
+++ algorithms/python/MoveZeroes/MoveZeroes.py
@@ -5,28 +5,37 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        if not nums or len(nums) == 1:
-            return
         p = 0
-        n = 0
         for i in range(0, len(nums)):
-            if nums[i] == 0:
-                n += 1
-            else:
-                if nums[p] != 0:
-                    p += 1
-                    n += 1
-                else:
-                    tmp = nums[p]
-                    nums[p] = nums[i]
-                    nums[i] = tmp
-                    p = n
+            if nums[i] != 0:
+                nums[p], nums[i] = nums[i], nums[p]
+                p += 1
 
+        # Print Result
         for n in nums:
             print(n, end = " ")
         print()
 
-Solution().moveZeroes([0,1,0,3,12])
+
+    def moveZeroes_2(self, nums: List[int]) -> None:
+        lastNoneZeroPosition = 0
+        # Move all the none zero elements to the front of the array
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[lastNoneZeroPosition] = nums[i]
+                lastNoneZeroPosition += 1
+
+        # Fill the rest elements of the array with zeros
+        for i in range(lastNoneZeroPosition, len(nums)):
+            nums[i] = 0
+
+        # Print Result
+        for n in nums:
+            print(n, end = " ")
+        print()
+
+
+Solution().moveZeroes_2([0,1,0,3,12])
 # expected: 1 3 12 0 0
 
 Solution().moveZeroes([2,1])
