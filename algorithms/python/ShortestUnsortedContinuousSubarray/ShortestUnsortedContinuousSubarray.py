c60b0a79b62703c62fc22f356729f7dcf5160b79
Heng Wang <kevinwang264@gmail.com>
2019-09-04 16:37:02 -0400

    update

--- algorithms/python/ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.py
+++ algorithms/python/ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.py
@@ -7,16 +7,17 @@ class Solution:
         begin = 0
         after = 0
         for i in range(1, len(nums)):
-            if nums[i] < nums[i-1]:
+            if nums[i] <= nums[i-1]:
                 begin = i-1
                 break
-        print(begin)
         for i in range(1, len(nums)):
-            if nums[i] < nums[i-1]:
+            if nums[i] <= nums[i-1]:
                 after = i
-        return after - begin + 1
+        return 0 if after == begin else after - begin + 1
 
-a = [2,6,4,8,10,9,15]
 
-l = Solution().findUnsortedSubarray(a)
-print(l)
+assert Solution().findUnsortedSubarray([2,6,4,8,10,9,15]) == 5
+assert Solution().findUnsortedSubarray([1,2,3,3,3]) == 0
+assert Solution().findUnsortedSubarray([1,3,2,2,2]) == 4
+
+print("OH YEAH!")
