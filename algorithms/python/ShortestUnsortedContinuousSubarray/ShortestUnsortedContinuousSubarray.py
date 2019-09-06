7d618d6d0118f04f3d513524b83712c81df95c38
Heng Wang <kevinwang264@gmail.com>
2019-09-06 14:13:56 -0400

    updated solution

--- algorithms/python/ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.py
+++ algorithms/python/ShortestUnsortedContinuousSubarray/ShortestUnsortedContinuousSubarray.py
@@ -1,6 +1,19 @@
 from typing import List
 
 class Solution:
+
+    # O(n*2)
+    def findUnsortedSubarray_brute_force(self, nums: List[int]) -> int:
+        left = len(nums)
+        right = 0
+        for i in range(0, len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[j] < nums[i]:
+                    left = min(left, i)
+                    right = max(right, j)
+        return 0 if right < left else right - left + 1
+
+
     def findUnsortedSubarray(self, nums: List[int]) -> int:
         if not nums or len(nums) == 1:
             return 0
@@ -17,10 +30,10 @@ class Solution:
         return 0 if right < left else right - left + 1
 
 
-assert Solution().findUnsortedSubarray([2,6,4,8,10,9,15]) == 5
-assert Solution().findUnsortedSubarray([1,2,3,3,3]) == 0
-assert Solution().findUnsortedSubarray([1,3,2,2,2]) == 4
-assert Solution().findUnsortedSubarray([1,2,3,4]) == 0
-assert Solution().findUnsortedSubarray([1,3,2,3,3]) == 2
+assert Solution().findUnsortedSubarray_brute_force([2,6,4,8,10,9,15]) == 5
+assert Solution().findUnsortedSubarray_brute_force([1,2,3,3,3]) == 0
+assert Solution().findUnsortedSubarray_brute_force([1,3,2,2,2]) == 4
+# assert Solution().findUnsortedSubarray([1,2,3,4]) == 0
+# assert Solution().findUnsortedSubarray([1,3,2,3,3]) == 2
 
 print("OH YEAH!")
